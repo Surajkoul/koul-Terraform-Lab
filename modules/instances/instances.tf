897d22a31dead15d1cd8656e5f60e40d52e4e88c
Surajkoul <166123609+Surajkoul@users.noreply.github.com>
2026-01-10 16:54:47 +0530

    Update instances.tf

--- modules/instances/instances.tf
+++ modules/instances/instances.tf
@@ -15,7 +15,7 @@ resource "google_compute_instance" "tf_instance_1" {
   network interface {
     network = "tf-vpc-620556"
     subnetwork = "projects/${var.project_id)/regions/us-east4/subnetworks/subnet-01"
-    access config {}
+    access config {}  #By default a VM in GCP only gets a private IP form the subnet, Adding access_config block tells the GCP to assign an external(public IP) address to the VM.#
   }
 
   metadata startup script <<-EOT
@@ -32,7 +32,7 @@ resource "google_compute_instance" "tf_instance_2" {
 
   boot disk {
     initialize params {
-      imsge = "debian-11-bullseye-v2051209"
+      image = "debian-11-bullseye-v2051209"
     }
   }
 
@@ -40,6 +40,7 @@ resource "google_compute_instance" "tf_instance_2" {
     network = "tf-vpc-620556"
     subnetwork = "projects/${var.project_id}/regions/us-east-4/subnetworks/subnet-01"
     access_config {}
+      nat_ip = google.compute_address.static_ip.address  ## This assigns a static external IP (reserver) instead of ephemeral.##
   }
 
   metadata_startup_script = <<-EOT
