e48e9fa31a0c72cd0ea84911da87769b31249c3a
Surajkoul <166123609+Surajkoul@users.noreply.github.com>
2026-01-10 16:51:12 +0530

    Create outputs.tf

---
+++ modules/instances/outputs.tf
@@ -0,0 +1,11 @@
+output "instance_1_name" {
+  value = google_compute_instance.tf_instance_1.name
+}
+
+output "instance_2_name" {
+  value = google_compute_instance.tf_instance_2.name
+}
+
+output "instance_3_name" {
+  value = google_compute_instance.tf_instance_1.name
+}
